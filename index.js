52a7cab77714b0e57023fa569a80705741975a25
Rody van der Jagt <rodyvanderj8@gmail.com>
2021-07-02 11:35:58 +0200

    set up CRUD in REST API

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const db = require('./queries.js')
+
 app.use(express.json())
 app.use(
   express.urlencoded({
@@ -9,11 +11,21 @@ app.use(
   })
 )
 
-app.get('/', (req, res) => {
-    res.json({info: 'Node.js, Express, and Postgres API'})
-});
+app.get('/', db.getUsers);
+
+app.get('/users/:id', db.getUserById);
+
+app.post('/users', db.createUser);
+
+app.put('/users/:id', db.updateUser);
+
+app.delete('/users/:id', db.deleteUser);
+
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`);
 });
+
+
+
 
