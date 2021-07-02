87ad693a705be087e19b4c4294df18b79cc0f88a
Rody van der Jagt <rodyvanderj8@gmail.com>
2021-07-02 10:34:59 +0200

    Set up Express.js server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (req, res) => {
+    res.json({info: 'Node.js, Express, and Postgres API'})
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`);
+});
+
